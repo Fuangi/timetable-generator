a49de4a8c8f778fac7ee9f9dd904a8037f17af61
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-07-08 15:23:50 +0100

    Linked the course table to the backend to fetch and display courses successfully

--- src/components/course/CourseTable.jsx
+++ src/components/course/CourseTable.jsx
@@ -1,25 +1,36 @@
+import { useEffect, useState } from "react";
 import TableRow from "../utils/TableRow";
+import axios from "axios";
 
 function CourseTable() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(function () {
+    axios({
+      url: "http://localhost:4000/timetable-ai/course",
+      method: "GET",
+    })
+      .then((res) => setCourses(res.data))
+      .catch((err) => console.log("Failed to create", err));
+  }, []);
+
   return (
     <table className="course-table">
       <thead>
         <tr>
           <th>SN</th>
+          <th>Course Code</th>
           <th>Course Name</th>
-          <th>Level</th>
+          <th>Hours/Week</th>
+          <th>Total Hours</th>
           <th>Specialties</th>
-          <th>Semester</th>
-          <th>Hours</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        <TableRow />
-        <TableRow />
-        <TableRow />
-        <TableRow />
-        <TableRow />
+        {courses?.map((course, i) => (
+          <TableRow key={i} index={i} data={course} />
+        ))}
       </tbody>
     </table>
   );
--- src/components/utils/TableRow.jsx
+++ src/components/utils/TableRow.jsx
@@ -1,14 +1,20 @@
 import { MdDelete, MdEdit } from "react-icons/md";
 
-function TableRow(data, index) {
+function TableRow(data) {
   return (
     <tr>
-      <td>1</td>
-      <td>Vanessa</td>
-      <td>Fuangi</td>
-      <td>try@gmail.com</td>
-      <td>123435676</td>
-      <td>Buea</td>
+      <td>{data.index + 1}</td>
+      <td>{data.data.course_code}</td>
+      <td>{data.data.name}</td>
+      <td>{data.data.hours_weekly}</td>
+      <td>{data.data.total_hours}</td>
+      <td>
+        {data.data.specialties.map((spe, i) => (
+          <span key={i} className="specialty">
+            {spe}
+          </span>
+        ))}
+      </td>
       <td>
         <button className="delete">
           <MdDelete />
